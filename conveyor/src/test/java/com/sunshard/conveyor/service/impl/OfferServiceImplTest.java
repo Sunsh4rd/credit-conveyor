852da733295bb756fe07fd221d0414a3ad0dfe31
Sunsh4rd <SsoslickK@gmail.com>
2023-08-10 19:56:44 +0400

    unit-test

--- conveyor/src/test/java/com/sunshard/conveyor/service/impl/OfferServiceImplTest.java
+++ conveyor/src/test/java/com/sunshard/conveyor/service/impl/OfferServiceImplTest.java
@@ -5,35 +5,38 @@ import com.sunshard.conveyor.model.LoanOfferDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class OfferServiceImplTest {
-
-//    @Mock
-//    private OfferRateCalculationService offerRateCalculationService;
-//
-//    @Mock
-//    private TotalAmountCalculationService totalAmountCalculationService;
-//
-//    @Mock
-//    private MonthlyPaymentCalculationService monthlyPaymentCalculationService;
-//
-//    @Mock
-//    private MonthlyRateCalculationService monthlyRateCalculationService;
-//
-//    @Mock
-//    private CreditProperties creditProperties;
-
     @InjectMocks
     private OfferServiceImpl offerService;
 
+    @Mock
+    private ScoringServiceImpl scoringService;
+
     @Test
     void createLoanOffers() {
-        List<LoanOfferDTO> loanOffers = offerService.createLoanOffers(new LoanApplicationRequestDTO());
+        List<LoanOfferDTO> loanOffers = offerService.createLoanOffers(
+                LoanApplicationRequestDTO.builder()
+                .amount(BigDecimal.valueOf(300000))
+                .term(18)
+                .firstName("first")
+                .lastName("last")
+                .middleName("middle")
+                .email("your@mail.com")
+                .birthdate(LocalDate.parse("2000-01-01"))
+                .passportSeries("1231")
+                .passportNumber("123456")
+                .build()
+        );
         assertEquals(4, loanOffers.size());
     }
 }
