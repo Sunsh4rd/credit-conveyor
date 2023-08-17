df3f8932b3a6bf61aee7ff486b9b0cfb441c5ff1
Sunsh4rd <SsoslickK@gmail.com>
2023-08-17 14:13:00 +0400

    add maven plugin

--- deal/src/main/java/com/sunshard/deal/mapper/ApplicationMapper.java
+++ deal/src/main/java/com/sunshard/deal/mapper/ApplicationMapper.java
@@ -12,14 +12,14 @@ public interface ApplicationMapper {
     ApplicationMapper INSTANCE = Mappers.getMapper(ApplicationMapper.class);
     Application dtoToEntity(ApplicationDTO applicationDTO);
 
-    @Mapping(target = "applicationId", source = "application.applicationId")
-    @Mapping(target = "client", source = "application.client")
-    @Mapping(target = "credit", source = "application.credit")
-    @Mapping(target = "status", source = "application.status")
-    @Mapping(target = "creationDate", source = "application.creationDate")
-    @Mapping(target = "appliedOffer", source = "application.appliedOffer")
-    @Mapping(target = "signDate", source = "application.signDate")
-    @Mapping(target = "sesCode", source = "application.sesCode")
-    @Mapping(target = "statusHistory", source = "application.statusHistory")
+//    @Mapping(target = "applicationId", source = "application.applicationId")
+//    @Mapping(target = "client", source = "application.client")
+//    @Mapping(target = "credit", source = "application.credit")
+//    @Mapping(target = "status", source = "application.status")
+//    @Mapping(target = "creationDate", source = "application.creationDate")
+//    @Mapping(target = "appliedOffer", source = "application.appliedOffer")
+//    @Mapping(target = "signDate", source = "application.signDate")
+//    @Mapping(target = "sesCode", source = "application.sesCode")
+//    @Mapping(target = "statusHistory", source = "application.statusHistory")
     ApplicationDTO entityToDto(Application application);
 }
--- deal/src/main/java/com/sunshard/deal/mapper/ClientMapper.java
+++ deal/src/main/java/com/sunshard/deal/mapper/ClientMapper.java
@@ -10,11 +10,11 @@ import org.mapstruct.factory.Mappers;
 public interface ClientMapper {
     ClientMapper INSTANCE = Mappers.getMapper(ClientMapper.class);
 
-    @Mapping(target = "firstName", source = "request.firstName")
-    @Mapping(target = "lastName", source = "request.lastName")
-    @Mapping(target = "middleName", source = "request.middleName")
-    @Mapping(target = "birthDate", source = "request.birthDate")
-    @Mapping(target = "email", source = "request.email")
+//    @Mapping(target = "firstName", source = "request.firstName")
+//    @Mapping(target = "lastName", source = "request.lastName")
+//    @Mapping(target = "middleName", source = "request.middleName")
+//    @Mapping(target = "birthDate", source = "request.birthDate")
+//    @Mapping(target = "email", source = "request.email")
     @Mapping(target = "passport.series", source = "request.passportSeries")
     @Mapping(target = "passport.number", source = "request.passportNumber")
     Client dtoToEntity(LoanApplicationRequestDTO request);
