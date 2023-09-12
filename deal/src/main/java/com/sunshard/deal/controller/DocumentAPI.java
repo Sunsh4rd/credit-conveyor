fc5edd96489db7c03c49523cd65b8eb12bc6d48b
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2023-09-12 17:31:12 +0400

    add apis

--- deal/src/main/java/com/sunshard/deal/controller/DocumentAPI.java
+++ deal/src/main/java/com/sunshard/deal/controller/DocumentAPI.java
@@ -1,8 +1,16 @@
 package com.sunshard.deal.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public interface DocumentAPI {
 
-//    @PostMapping("/deal/{applicationId}/send")
+    @PostMapping("/deal/document/{applicationId}/send")
+    ResponseEntity<Void> sendDocuments();
+
+    @PostMapping("/deal/document/{applicationId}/sign")
+    ResponseEntity<Void> signRequest();
+
+    @PostMapping("/deal/document/{applicationId}/code")
+    ResponseEntity<Void> signDocuments();
 }
